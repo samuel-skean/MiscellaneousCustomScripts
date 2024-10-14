86854575e43582353b83a3b59e96b00f3a75f624
Samuel Skean <sskean2@uic.edu>
2024-10-13 22:02:55 -0500

    Switched to ~/scripts folder.

--- merge-empty-pdf.sh
+++ merge-empty-pdf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source ~/bin/.venv/bin/activate
-generate-empty-pdf.py $1
+source ~/scripts/.venv/bin/activate
+~/scripts/generate-empty-pdf.py $1
 pdftk A=$2 B=empty.pdf shuffle A Bend-1 output merged.pdf
 rm empty.pdf
