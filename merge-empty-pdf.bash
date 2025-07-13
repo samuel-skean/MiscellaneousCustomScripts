096bee53f1c89e7eab13efbc2f7653aa609109bc
Samuel Skean <sskean2@uic.edu>
2025-07-13 01:49:08 -0500

    Some fixes.
    These scripts are slightly less dependent on their old home,
    `~/.local/bin/`. Fully not-dependent? Seems so.

--- merge-empty-pdf.bash
+++ merge-empty-pdf.bash
@@ -2,7 +2,7 @@
 
 echo "TODO: make this script invoke things based on paths relative to the location of this script." >&2
 
-source ~/.local/bin/.venv/bin/activate
+source ~/scripts/.venv/bin/activate
 ~/scripts/generate-empty-pdf.py $1
 pdftk A=$2 B=empty.pdf shuffle A Bend-1 output merged.pdf
 rm empty.pdf
